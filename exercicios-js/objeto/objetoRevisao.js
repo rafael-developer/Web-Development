6e6503b52f8351860f16c9d5d141cdec6a09f076
Rafael <rgildo38@gmail.com>
2019-06-16 23:48:13 -0300

    day 6/100

---
+++ exercicios-js/objeto/objetoRevisao.js
@@ -0,0 +1,45 @@
+// Objeto é coleção dinâmica de pares chave/valor
+
+// produto instancia da classe Object. new metodo construtor da classe Object
+const produto = new Object
+produto.nome = 'Cadeira'
+produto['marca do produto'] = 'Generica'
+produto.preco = 220
+
+console.log(produto)
+delete produto.preco
+delete produto['marca do produto']
+console.log(produto)
+
+const carro = {
+    modelo: 'A4',
+    valor: 89000,
+    propriedade: {
+        nome: 'Raul',
+        idade: 56,
+        endereco: {
+            logradouro: 'Rua ABC',
+            numero: 123
+        }
+    },
+    condutores: [{
+        nome: 'Junior',
+        idade: 19
+    }, {
+        nome: 'Ana',
+        idade: 42
+    }],
+    calcularValorSeguro: function() {
+        // ...
+    }
+}
+carro.propriedade.endereco.numero = 1000
+carro['proprietario']['endereco']['logradouro'] = 'Av Gigante'
+console.log(carro)
+
+// delete carro.condutores
+delete carro.proprietario.endereco
+delete carro.calcularValorSeguro
+console.log(carro)
+console.log(carro.condutores) // undefined
+console.log(carro.condutores.length) // erro
---
+++ exercicios-js/objeto/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser objetos
+
+// PRINCÍPIOS IMPORTANTES:
+// 1. ABSTRAÇÃO - informações pertinentes à aplicação
+// 2. ENCAPSULAMENTO 
+// 3. HERANÇA (PROTOTYPE)
+// 4. POLIMORFISMO
