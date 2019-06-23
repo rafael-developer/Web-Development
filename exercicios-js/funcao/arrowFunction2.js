3c994aaa1280e3fe40fcd59e66807936e4d563e6
Rafael <rgildo38@gmail.com>
2019-06-22 22:38:23 -0300

    day 9/100

--- exercicios-js/funcao/arrowFunction2.js
+++ exercicios-js/funcao/arrowFunction2.js
@@ -2,8 +2,8 @@ function Pessoa() {
     this.idade = 0
 
     setInterval(() => {
-        this.idade++
-        console.log(this.idade)
+        //this.idade++
+        console.log(this.idade++)
     }, 1000)
 }
 
---
+++ exercicios-js/praticando/objetos.js
@@ -0,0 +1,6 @@
+const umObj = {
+    nome: 'Rafael',
+    nasc: '24/03/1993',
+    idade: 26
+}
+console.log(umObj)
