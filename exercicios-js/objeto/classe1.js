7fb378ccc347b6c8136aa0985569ff3b5cd928f8
Rafael <rgildo38@gmail.com>
2019-06-26 14:50:59 -0300

    day 12/100

--- exercicios-js/objeto/classe1.js
+++ exercicios-js/objeto/classe1.js
@@ -1,3 +1,5 @@
+// RECEBE OS LANÇAMENTOS DAS CONTAS
+
 class Lancamento {
     constructor(nome = 'Genérico', valor = 0) {
         this.nome = nome
@@ -5,29 +7,33 @@ class Lancamento {
     }
 }
 
+// RECEBE O MÊS E ANO CORRENTE
 class CicloFinanceiro {
     constructor (mes, ano) {
         this.mes = mes
         this.ano = ano
-        this.lancamentos = []
+        this.lancamentos = []   // RECEBE O SPREAD DO METODO .addLancamentos()
     }
 
-    addLancamentos(...lancamentos) {
-        lancamentos.forEach(l => this.lancamentos.push(l))
+    addLancamentos(...lancamentos) {    // Vai receber objetos da Classe Lancamentos
+        lancamentos.forEach(l => this.lancamentos.push(l)) // Concatena os objetos no array this.lancamentos[]
     }
 
     sumario() {
         let valorConsolidado = 0
-        this.lancamentos.forEach(l => {
-            valorConsolidado += l.valor
-        })
+        this.lancamentos.forEach(l =>  { valorConsolidado += l.valor })
+        //  Faz o somatório com os atributos [valor] dos objetos recebidos por .addLancamentos()
         return valorConsolidado
     }
 }
 
+// Cria duas instâncias da Classe Lancamento
 const salario = new Lancamento('Salario', 45000)
 const contaDeLuz = new Lancamento('Luz', -220)
 
+// Instancia o ciclo Corrente
 const contas = new CicloFinanceiro(6, 2018)
+//  Faz o lançamentos das contas do mês na instância criada
 contas.addLancamentos(salario, contaDeLuz)
+//Executa o somatório
 console.log(contas.sumario())
---
+++ exercicios-js/praticando/heranca.js
@@ -0,0 +1,6 @@
+var Homem = function (nome, idadee) {
+    this.nome = nome;
+    this.idade = idade;
+};
+
+Homem.prototype.sexo = "masculino";
