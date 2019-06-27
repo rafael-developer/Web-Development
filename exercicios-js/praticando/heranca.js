a09e7033a257393621277e17180743692bc358c0
Rafael <rgildo38@gmail.com>
2019-06-27 00:08:21 -0300

    Update heranca.js

--- exercicios-js/praticando/heranca.js
+++ exercicios-js/praticando/heranca.js
@@ -1,6 +1,19 @@
-var Homem = function (nome, idadee) {
+var Homem = function (nome, idade) {
     this.nome = nome;
     this.idade = idade;
 };
 
 Homem.prototype.sexo = "masculino";
+
+var joao = new Homem("João", 20);
+console.log(joao);
+console.log(joao.sexo);
+
+var pedro = {};
+pedro.__proto__ = Homem.prototype;
+Homem.apply(pedro, ["Pedro", 18]);
+console.log(pedro)
+console.log(pedro.sexo);
+Homem.prototype.sexo = "feminino";
+console.log(joao. sexo);
+console.log(pedro.sexo);
