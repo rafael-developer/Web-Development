d03fd6676b1a98d7ec04f9df2dbf298bb0c03cd6
RafaelMath <rgildo38@gmail.com>
2020-04-09 22:08:13 -0300

    final Ajax

--- exercicios-js/ajax/server.js
+++ exercicios-js/ajax/server.js
@@ -37,4 +37,14 @@ app.post('/formulario', (req, res) => {
 
 
 
+app.get('/parOuImpar', (req, res) => {
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
+
+
+
 app.listen(8081, console.log('executando'));
