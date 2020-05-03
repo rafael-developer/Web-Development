26dbf9c05ff69b9a8c34c69d8ae80c790ab4fc0e
RafaelMath <rgildo38@gmail.com>
2020-05-03 12:24:22 -0300

    React-childre

--- exercicios-js/react/exercicios/src/index.js
+++ exercicios-js/react/exercicios/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Saudacao from './componentes/Saudacao'
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDOM.render(
     <div>
-        <Saudacao tipo="Bom dia" nome="Rafael"/>
+        <Pai>
+            <Filho nome="Rafael" sobrenome="Gildo" />
+        </Pai>
     </div>
 , document.getElementById('root'));
